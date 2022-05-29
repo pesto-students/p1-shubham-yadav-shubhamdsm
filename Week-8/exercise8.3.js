57db37e889525678699e7b8db097450f22cc1acc
Shubham <shubhamdv19@gmail.com>
2022-05-29 11:29:40 +0530

    added week8

---
+++ Week-8/exercise8.3.js
@@ -0,0 +1,28 @@
+var levelOrder = function (root) {
+  const levels = [];
+
+  if (!root) {
+    return levels;
+  }
+
+  const queue = [root];
+  while (queue.length) {
+    const queueLength = queue.length;
+    const level = [];
+
+    for (let i = 0; i < queueLength; i++) {
+      const node = queue.shift();
+
+      if (node.left) {
+        queue.push(node.left);
+      }
+      if (node.right) {
+        queue.push(node.right);
+      }
+
+      level.push(node.val);
+    }
+    levels.push(level);
+  }
+  return levels;
+};
