df1ed88eebcaa8c78fcc3d31f270c27e6d4d93ea
Shubham <shubhamdv19@gmail.com>
2022-06-08 00:18:50 +0530

    week10 submission

---
+++ Week-10/shortly/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Main from './components/Main'
+import Cards from './components/Cards'
+import Footer from './components/Footer'
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Hero />
+      <Main />
+      <Cards />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
